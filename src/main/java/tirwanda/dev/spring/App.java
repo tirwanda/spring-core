9649684c9afcf83a9d6f67885957006e996ce2a3
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-07-03 23:45:07 +0700

    First Commit

---
+++ src/main/java/tirwanda/dev/spring/App.java
@@ -0,0 +1,13 @@
+package tirwanda.dev.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
