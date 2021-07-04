5470b66feb0d9c557ba413f2e3795a22fe0ecebd
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-07-04 21:33:22 +0700

    Methode Setter from applicationContext.XML

--- src/main/java/tirwanda/dev/spring/App.java
+++ src/main/java/tirwanda/dev/spring/App.java
@@ -5,10 +5,6 @@ import tirwanda.dev.spring.service.AccountService;
 
 import java.math.BigDecimal;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
--- src/main/java/tirwanda/dev/spring/repository/impl/AccountRepositoryImpl.java
+++ src/main/java/tirwanda/dev/spring/repository/impl/AccountRepositoryImpl.java
@@ -3,29 +3,11 @@ package tirwanda.dev.spring.repository.impl;
 import tirwanda.dev.spring.entity.Account;
 import tirwanda.dev.spring.repository.AccountRepository;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.Map;
 
 public class AccountRepositoryImpl implements AccountRepository {
 
-    private Map<Long, Account> accountMap = new HashMap<>();
-    {
-        Account account1 = new Account();
-        account1.setId(1L);
-        account1.setAccountNumber("001");
-        account1.setBalance(new BigDecimal(100_000));
-        account1.setName("Edho");
-
-        Account account2= new Account();
-        account2.setId(2L);
-        account2.setAccountNumber("002");
-        account2.setBalance(new BigDecimal(200_000));
-        account2.setName("Dwi");
-
-        accountMap.put(account1.getId(), account1);
-        accountMap.put(account2.getId(), account2);
-    }
+    private Map<Long, Account> accountMap;
 
     @Override
     public void update(Account account) {
@@ -35,5 +17,13 @@ public class AccountRepositoryImpl implements AccountRepository {
     @Override
     public Account find(Long id) {
         return accountMap.get(id);
+    }
+
+    public Map<Long, Account> getAccountMap() {
+        return accountMap;
+    }
+
+    public void setAccountMap(Map<Long, Account> accountMap) {
+        this.accountMap = accountMap;
     }
 }
